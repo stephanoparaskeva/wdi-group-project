a85ef6242ced4b80a49de3324f28a4dae5b30afe
Matthew Whaley <mjwhaley@mac.com>
2019-03-25 17:45:14 +0000

    Bulma changes

--- src/components/groups/group.js
+++ src/components/groups/group.js
@@ -14,28 +14,34 @@ class Group extends React.Component {
   render() {
     return(
       <div className="card">
-        <header className="card-header">
-          <p className="card-header-title is-centered">
-            {`${this.props.name}`} Group
-          </p>
-        </header>
-        <div className="card-content">
-          <div className="content">
-            <p>{this.props.description}</p>
-            <br />
-            <p>Created by: Mary</p>
-            <p>{`Priority: ${this.props.priority}`}</p>
-            <p>{`Description: ${this.props.description}`}</p>
-            <p>{`Users: ${this.props.usersAssigned}`}</p>
+        <div className="card-header">
+          <div className="card-header-title is-centered is-size-3">
+            {this.props.name}
           </div>
         </div>
-        <footer className="card-footer">
+        <div className="card-content">
+          <div className="content">
+            <div className="subtitle is-size-4">{this.props.description}</div>
+            <hr />
+            <div>
+              <content className="content is-6">{`Users: ${this.props.usersAssigned}`}</content>
+            </div>
+            <div>
+              <content className="subtitle is-6">{`User ID: ${this.props.createdBy}`}</content>
+            </div>
+            <div>
+              <content className="subtitle is-6">{`Created date: ${this.props.createdAt}`}</content>
+            </div>
+
+          </div>
+        </div>
+        <div className="card-footer">
           <a href="#" className="card-footer-item">Edit</a>
           <a href="#" className="card-footer-item">Delete</a>
           <Link to={`/groups/${this.props._id}/tasks`} className="card-footer-item">
-            <strong>Details</strong>
+            <strong>Tasks</strong>
           </Link>
-        </footer>
+        </div>
       </div>
     )
   }
