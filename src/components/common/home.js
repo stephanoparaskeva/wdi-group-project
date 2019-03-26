9d7aff11c5e9a849f5dac3ea7a5beaef490e367f
Matthew Whaley <mjwhaley@mac.com>
2019-03-26 21:30:15 +0000

    updated pages

---
+++ src/components/common/home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'bulma'
+
+class Home extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <p className="title is-1">Organise/homepage</p>
+            <hr />
+            <p className="subtitle is-4"><strong>Homepage content:</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu ex ullamcorper, volutpat dolor sed, ultricies ligula. Etiam laoreet nisl semper mi scelerisque, molestie pellentesque metus pulvinar. Quisque consectetur venenatis neque et pellentesque. Phasellus laoreet sit amet elit quis finibus. Mauris eu elit odio. Vivamus ac mauris ligula. Fusce nibh urna, molestie eu magna quis, dictum congue metus. Integer vitae dignissim erat, et auctor erat. Sed ut finibus urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/groups/groupsMatt.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import 'bulma'
+import axios from 'axios'
+import Group from './group'
+import CreateGroup from './createGroup'
+
+class Groups extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      groups: []
+    }
+
+    this.getAllGroups = this.getAllGroups.bind(this)
+    this.getAllGroupsForUpdate = this.getAllGroupsForUpdate.bind(this)
+    this.childRerendersPage = this.childRerendersPage.bind(this)
+  }
+
+  getAllGroups() {
+    axios.get('/api/groups', {
+    })
+      .then(res => this.setState({ groups: res.data }))
+  }
+
+  getAllGroupsForUpdate() {
+    axios.get('/api/groups', {
+    })
+      .then(res => {
+        if(res.data.length !== this.state.groups.length) {
+          this.setState({ groups: res.data })
+        }
+      })
+  }
+
+  componentDidMount() {
+    this.getAllGroups()
+  }
+  componentDidUpdate() {
+    this.getAllGroupsForUpdate()
+  }
+
+  childRerendersPage() {
+    this.setState(this.state)
+  }
+
+  render() {
+    if(!this.state.groups) return null
+    return(
+      <div className="section">
+        <div className="container">
+          <div className="columns is-mobile is-multiline">
+            {this.state.groups.map(group =>
+              <Group {...group} key={group._id} />
+            )}
+            <CreateGroup rerender={this.childRerendersPage}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Groups
